a79a79c5f4f931d25123017a52bcdcead102f7ac
Esben Horn <esbenhorn@Esbens-MacBook-Air-3.local>
2017-03-16 20:58:48 +0100

    Implement parser functions for operations and expressions

--- kravl-parser/src/syntax/ast.rs
+++ kravl-parser/src/syntax/ast.rs
@@ -1,6 +1,6 @@
 use syntax::tokens::{
-    Token,
     TokenType,
+    BinOp,
 };
 
 use syntax::lexer::Lexer;
@@ -28,13 +28,14 @@ pub enum Expression {
     Integer(i64),
     Float(f64),
     Text(String),
+    Bool(bool),
     Call(String, Box<Vec<Expression>>),
     Assignment(Box<Expression>, Box<Expression>),
     Dot(Box<Expression>, Box<Expression>),
     Index(String, Box<Expression>),
     Array(Box<Vec<Expression>>),
-    True,
-    False,
+    Identifier(String),
+    Operation(Box<Expression>, BinOp, Box<Expression>),
 }
 
 pub struct Parser {
@@ -48,7 +49,69 @@ impl Parser {
         }
     }
 
-    fn parse_current(&mut self) -> Result<Expression, String> {
+    pub fn from(lexer: Lexer) -> Parser {
+        Parser {
+            lexer: lexer,
+        }
+    }
+
+    fn parse_bin_op(&mut self, expr: Expression) -> Result<Expression, String> {
+        let mut expr_list = vec!(expr);
+        let mut oper_list: Vec<(BinOp, u8)> = Vec::new();
+
+        oper_list.push(Lexer::bin_op(&self.lexer.current_token_content()[..]).unwrap());
+
+        self.lexer.next_token();
+
+        expr_list.push(try!(self.parse_word()));
+
+        let mut done = false;
+
+        while expr_list.len() > 1 {
+
+            let left = expr_list.pop().unwrap();
+            let right = expr_list.pop().unwrap();
+
+            let oper = Expression::Operation(
+                Box::new(left),
+                oper_list.pop().unwrap().0,
+                Box::new(right),
+            );
+
+
+            if !done && self.lexer.next_token() {
+                if self.lexer.current_token().token_type != TokenType::BinOp {
+                    self.lexer.previous_token();
+                    done = false;
+
+                    continue
+                }
+
+                let (op, prec) = Lexer::bin_op(&self.lexer.current_token_content()[..]).unwrap();
+
+                if prec > oper_list.last().unwrap().1 {
+                    expr_list.push(oper);
+
+                    self.lexer.next_token();
+
+                    expr_list.push(try!(self.parse_word()));
+                    oper_list.push((op, prec));
+
+                    continue
+                }
+                self.lexer.next_token();
+
+                expr_list.push(try!(self.parse_word()));
+                oper_list.push((op, prec));
+            }
+
+            expr_list.push(oper);
+        }
+
+        Ok(expr_list.pop().unwrap())
+    }
+
+    fn parse_word(&mut self) -> Result<Expression, String> {
         match self.lexer.current_token().token_type {
             TokenType::Integer => {
                 Ok(Expression::Integer(
@@ -69,16 +132,50 @@ impl Parser {
             },
 
             TokenType::True => {
-                Ok(Expression::True)
+                Ok(Expression::Bool(true))
             },
 
             TokenType::False => {
-                Ok(Expression::False)
+                Ok(Expression::Bool(false))
+            },
+
+            TokenType::Identifier => {
+                let id = Expression::Identifier(self.lexer.current_token_content());
+
+                if self.lexer.next_token() {
+                    return match self.lexer.current_token().token_type {
+                        TokenType::BinOp => {
+                            self.parse_bin_op(id)
+                        },
+                        _ => {
+                            self.lexer.previous_token();
+                            Ok(id)
+                        }
+                    }
+                }
+
+                Ok(id)
             },
 
             _ => {
-                Ok(Expression::False)
+                Ok(Expression::Bool(false))
             }
         }
+    }
+
+    fn parse_expression(&mut self) -> Result<Expression, String> {
+        let expr = try!(self.parse_word());
+
+        self.lexer.next_token();
+
+        if self.lexer.tokens_remaining() > 0 {
+            if self.lexer.current_token().token_type == TokenType::BinOp {
+                return self.parse_bin_op(expr);
+            }
+
+            self.lexer.previous_token();
+        }
+
+        Ok(expr)
     }
 }
--- kravl-parser/src/syntax/mod.rs
+++ kravl-parser/src/syntax/mod.rs
@@ -19,4 +19,21 @@ mod tests {
 
         assert!(lexer.get_tokens().len() != 0)
     }
+
+    #[test]
+    fn parser_tokenize() {
+        use syntax::lexer::Lexer;
+        use syntax::ast::Parser;
+
+        let mut lexer = Lexer::new();
+
+        lexer.tokenize(String::from("
+            define fib_sum(a -> int, b -> int) -> int
+                if a + b <= 2
+                    return c
+                fib_sum(a, b - 1) + fib_sum(a, b - 2)
+        "));
+
+        let mut parser = Parser::from(lexer);
+    }
 }
